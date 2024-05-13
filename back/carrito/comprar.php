3b05c1aa298ee9f0ffc6741b3d9371f033ca4bac
Pavlin Dodov <paulins.dod@educa.jcyl.es>
2024-05-13 23:46:03 +0200

    try-catch añadido

--- back/carrito/comprar.php
+++ back/carrito/comprar.php
@@ -8,45 +8,49 @@
     $precioTotal = $_GET['precioTotal'];
 
     $conexion = conectar();
+    try {
+        if (isset($_SESSION['carrito']) && !empty($_SESSION['carrito'])) {
+            beginTransaction($conexion);
 
-    if (isset($_SESSION['carrito']) && !empty($_SESSION['carrito'])) {
-        beginTransaction($conexion);
+            $consulta = "INSERT INTO factura (totalFactura, dniCliente, fechaFactura) VALUES ('0', '$dni', now())";
+            $conexion->query($consulta);
 
-        $consulta = "INSERT INTO factura (totalFactura, dniCliente, fechaFactura) VALUES ('0', '$dni', now())";
-        $conexion->query($consulta);
+            $consulta = "SELECT * FROM factura ORDER BY id DESC LIMIT 1";
+            $resultado = $conexion->query($consulta);
+            $registro = $resultado->fetch_assoc();
+            $idFactura = $registro['id'];
 
-        $consulta = "SELECT * FROM factura ORDER BY id DESC LIMIT 1";
-        $resultado = $conexion->query($consulta);
-        $registro = $resultado->fetch_assoc();
-        $idFactura = $registro['id'];
+            foreach ($_SESSION['carrito'] as $i => $producto) {
+                if ($producto['cantidad'] > $producto['stock']) {
+                    throw new Exception();
+                }
 
-        foreach ($_SESSION['carrito'] as $i => $producto) {
-            if ($producto['cantidad'] > $producto['stock']) {
-                rollback($conexion);
-                header("location:carrito.php?updateCarrito=5");
-                exit();
+                $cantidadVendida = $producto['cantidad'];
+                $stock = $producto['stock'];
+                $idProducto = $producto['id'];
+                $cantidadRestante = $stock - $cantidadVendida;
+                $precioProducto = $producto['precioProducto'];
+
+                $consulta = "INSERT INTO lineafactura VALUES ('$idFactura', '$idProducto', '$cantidadVendida', '$precioProducto')";
+                $conexion->query($consulta);
+
+                $consulta = "UPDATE producto SET stock='$cantidadRestante' WHERE id='$idProducto'";
+                $conexion->query($consulta);
             }
 
-            $cantidadVendida = $producto['cantidad'];
-            $stock = $producto['stock'];
-            $idProducto = $producto['id'];
-            $cantidadRestante = $stock - $cantidadVendida;
-            $precioProducto = $producto['precioProducto'];
-
-            $consulta = "INSERT INTO lineafactura VALUES ('$idFactura', '$idProducto', '$cantidadVendida', '$precioProducto')";
+            $consulta = "UPDATE factura SET totalFactura = '$precioTotal' WHERE id='$idFactura'";
             $conexion->query($consulta);
 
-            $consulta = "UPDATE producto SET stock='$cantidadRestante' WHERE id='$idProducto'";
-            $conexion->query($consulta);
+            commit($conexion);
         }
 
-        $consulta = "UPDATE factura SET totalFactura = '$precioTotal' WHERE id='$idFactura'";
-        $conexion->query($consulta);
+        desconectar($conexion);
+        header("location:eliminarCarrito.php?origen=comprar");
+        exit();
 
-        commit($conexion);
+    } catch (Exception $e) {
+        rollback($conexion);
+        header("location:carrito.php?updateCarrito=5");
+        exit();
     }
-
-    desconectar($conexion);
-    header("location:eliminarCarrito.php?origen=comprar");
-    exit();
 ?>
